bd758c3b5fc3e9784d2bba2e609aec6f3e3ec2b8
Leonardo Domingos Patricio <leonardo_patricio@estudante.sesisenai.org.br>
2024-05-07 20:37:02 -0300

    emplementando o controller, mapper,service de cidade

---
+++ src/main/java/com/barbershop/barbershop/cidade/CidadeController.java
@@ -0,0 +1,48 @@
+package com.barbershop.barbershop.cidade;
+
+import com.barbershop.barbershop.diaSemana.DiaSemanaDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+public class CidadeController {
+
+    @Autowired
+    private CidadeService cidadeService;
+
+    @GetMapping
+    public ResponseEntity<List<CidadeDTO>> getAll(){
+        List<CidadeDTO> cidadesDTO = cidadeService.findAll();
+        return ResponseEntity.ok(cidadesDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CidadeDTO> getById(@PathVariable Integer id){
+        CidadeDTO cidadeDTO = cidadeService.findById(id);
+        return ResponseEntity.ok(cidadeDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CidadeDTO> createDiaSemana(@Valid @RequestBody CidadeDTO cidadeDTO){
+        CidadeDTO create = cidadeService.create(cidadeDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(create);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CidadeDTO> update(@PathVariable Integer id, @Valid @RequestBody CidadeDTO cidadeDTO){
+        CidadeDTO update= cidadeService.update(id, cidadeDTO);
+        return ResponseEntity.ok(update);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Valid> dalete(@PathVariable Integer id){
+        cidadeService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
