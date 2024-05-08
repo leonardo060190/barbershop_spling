c1372c77d86a1009716698f0df0ce6e30a010ec3
leonardo060190 <leonardo_patricio@estudante.sesisenai.org.br>
2024-05-08 16:52:11 -0300

    ok

--- src/main/java/com/barbershop/barbershop/cidade/CidadeMapper.java
+++ src/main/java/com/barbershop/barbershop/cidade/CidadeMapper.java
@@ -1,4 +1,30 @@
 package com.barbershop.barbershop.cidade;
 
+import com.barbershop.barbershop.estado.Estado;
+import com.barbershop.barbershop.estado.EstadoDTO;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
 public interface CidadeMapper {
+
+    //Conversor Entity(Model) para DTO
+    @Mapping(source = "id", target = "id")
+    CidadeDTO toDTO(Cidade cidade);
+
+    @Mapping(source = "id", target = "id")
+    Cidade toEntity(CidadeDTO cidadeDTO );
+
+    //recebendo uma lista do estados
+    List<CidadeDTO> toDTO(List<Cidade> cidades);
+
+    //troca os dados de EstadoDTO par Estado(entidade)
+    @Mappings({
+            @Mapping(source = "cidadeDTO.id", target = "id"),
+            @Mapping(source = "cidadeDTO.nome", target = "nome"),
+            @Mapping(source = "cidadeDTO.dataCriacao", target = "dataCriacao"),
+    })
+    Estado updateEntity(CidadeDTO cidadeDTO, Cidade cidade);
+
 }
--- src/main/java/com/barbershop/barbershop/cidade/CidadeService.java
+++ src/main/java/com/barbershop/barbershop/cidade/CidadeService.java
@@ -1,10 +1,56 @@
 package com.barbershop.barbershop.cidade;
 
+import com.barbershop.barbershop.estado.Estado;
+import com.barbershop.barbershop.estado.EstadoDTO;
+import com.barbershop.barbershop.estado.EstadoMapper;
+import com.barbershop.barbershop.estado.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CidadeService {
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private CidadeMapper cidadeMapper;
+
+
+    //buscar todos s estados
+
+    public List<CidadeDTO> findAll(){
+        List<Cidade> cidades = cidadeRepository.findAll();
+        return cidades.stream().map(cidadeMapper::toDTO).collect(Collectors.toList());
+    }
+
+    //buscar por id
+    public CidadeDTO findById(Integer id){
+        Cidade cidade = cidadeRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Cidade não encotrado"));
+        return cidadeMapper.toDTO(cidade);
+    }
+
+    //criando um novo estado
+    public CidadeDTO create(CidadeDTO cidadeDTO){
+        Cidade cidade = cidadeMapper.toEntity(cidadeDTO);
+        cidade = cidadeRepository.save(cidade);
+        return cidadeMapper.toDTO(cidade);
+    }
+
+    //update estado
+    public CidadeDTO update(Integer id, CidadeDTO cidadeDTO){
+        Cidade cidade = cidadeRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Cidade não encotrado"));
+        cidadeMapper.updateEntity(cidadeDTO,cidade);
+        cidade = cidadeRepository.save(cidade);
+        return cidadeMapper.toDTO(cidade);
+    }
+
+    public void deleteById(Integer id){
+        cidadeRepository.deleteById(id);
+    }
 
 
 }
